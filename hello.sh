c5648828374711abbc18cd7cf2247c69e2c96d5e
Devendra <112802360+DEVENDRA-5470@users.noreply.github.com>
2026-07-09 17:37:04 +0530

    Add nginx installation and version check
    Added commands to install and check nginx version.

--- hello.sh
+++ hello.sh
@@ -3,3 +3,7 @@ echo "User : $(whoami)"
 echo "Hostname : $(hostname)"
 echo "Current Directory : $(pwd) "
 echo "Date : $(date)"
+echo "Installing Updates"
+sudo apt update
+sudo apt install -y nginx
+nginx --version
